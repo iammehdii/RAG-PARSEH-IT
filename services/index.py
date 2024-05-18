82a807ca17ed87a972f4a168a4714629ade36168
Mehdi <35371909+iammehdii@users.noreply.github.com>
2024-05-19 00:42:13 +0330

    added format_input function

--- services/index.py
+++ services/index.py
@@ -1,12 +1,10 @@
-from langchain.document_loaders import AsyncHtmlLoader
 import pandas as pd
-# from langchain_community.document_transformers import BeautifulSoupTransformer
-# from langchain_community.document_transformers import BeautifulSoupTransformer
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.docstore.document import Document
 from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+
+load_dotenv('.env')
 
 question_templates = {
     "crops": "Which crops are affected by {disease_name}?",
@@ -22,6 +20,27 @@ def load_local_db(path='db/plantix_faiss'):
     """Load local vector store"""
     load_dotenv('.env')
     return FAISS.load_local(path, OpenAIEmbeddings(), allow_dangerous_deserialization=True)
+
+
+def format_input(items_in_dict,question_templates=question_templates):
+    """Add items to db"""
+    qa_pairs = []
+    disease_name = items_in_dict['name_en']
+    for key, question_template in question_templates.items():
+        question = question_template.format(disease_name=disease_name)
+        answer = items_in_dict[key]
+        if isinstance(answer, str):
+            qa_pair = question + '\n' + answer
+        elif isinstance(answer, list):
+            qa_pair = question + '\n' + ' '.join(answer)
+        else:
+            continue
+        doc = Document(page_content=qa_pair,metadata={"source": items_in_dict.get('url', None)})
+
+        qa_pairs.append(doc)
+    return qa_pairs
+
+db = FAISS.load_local('db/plantix_faiss', OpenAIEmbeddings(), allow_dangerous_deserialization=True)
 
 
 if __name__ == "__main__":
