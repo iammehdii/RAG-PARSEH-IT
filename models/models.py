ccd46a98d6b4d6094ecdc0ad3eba675f2747c8ef
Mehdi <35371909+iammehdii@users.noreply.github.com>
2024-05-19 00:44:08 +0330

    add pydantic class for data validation

---
+++ models/models.py
@@ -0,0 +1,23 @@
+from langchain_core.pydantic_v1 import BaseModel
+from typing import List, Optional, Literal
+
+
+class DiseaseItem(BaseModel):
+    disease_name: str
+    crops: List[str]
+    symptoms: str
+    organic_control: str
+    chemical_control: str
+    trigger: str
+    preventive_measures: str
+    url: Optional[str] = None  # Assuming URL is optional, remove Optional if it's mandatory
+
+
+class Question(BaseModel):
+    __root__: str
+
+
+class TopicClassifier(BaseModel):
+    """Classify the topic of the user question"""
+    topic: Literal["agriculture", "general"]
+    "The topic of the user question. One of 'agriculture', or 'general'."
